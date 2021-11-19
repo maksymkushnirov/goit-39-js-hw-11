fe40bf9dd3a50b021b62e43c9c00e128d3abe203
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-19 14:26:04 +0200

    library settings: notiflix

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,68 @@
-import { fetchImages } from './api.js';
+import { fetchImages } from './services/api.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more-btn');
+let gallery;
+let page = 1;
+const pageSize = 40;
+
+loadMoreBtnEl.classList.add('is-hidden');
 
 formEl.addEventListener('submit', onSearchSubmit);
+loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
 
 function onSearchSubmit(e) {
   e.preventDefault();
+  page = 1;
+  document.querySelector('.limit-reached')?.remove();
   galleryEl.innerHTML = '';
+  makeRequest();
+}
+
+function onLoadMoreBtnClick() {
+  gallery.destroy();
+  makeRequest();
+}
+
+function makeRequest() {
+  loadMoreBtnEl.classList.add('is-hidden');
   getImages();
+  page += 1;
 }
 
 function getImages() {
   const query = formEl.elements.searchQuery.value.trim();
-  fetchImages(query)
-    .then(({ data }) => {
-      if (data.hits.length === 0) {
-      }
-      renderGallery(data.hits);
-    })
-    .catch(error => console.log(error));
+
+  if (query !== '') {
+    fetchImages(query, page, pageSize)
+      .then(({ data }) => {
+        if (data.hits.length === 0) {
+          Notify.failure(
+            `Sorry, there are no images matching your search query. Please try again.`,
+          );
+        } else {
+          if (page === 2) {
+            Notify.success(`Hooray! We found ${data.totalHits} images.`);
+          }
+
+          renderGallery(data.hits);
+
+          loadMoreBtnEl.classList.remove('is-hidden');
+
+          const restOfImages = data.totalHits - (page - 1) * pageSize;
+          restOfImages < 1 && stopLoadMore();
+        }
+      })
+      .catch(error => console.log(error));
+  }
 }
 
-function createMarkup(images) {
-  return images
+function renderGallery(images) {
+  const markup = images
     .map(image => {
       const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
       return `<a class="gallery__link" href="${largeImageURL}">
@@ -51,47 +88,7 @@ function createMarkup(images) {
       </a>`;
     })
     .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  gallery = new SimpleLightbox('.gallery a');
 }
-
-function renderGallery(images) {
-  galleryEl.insertAdjacentHTML('beforeend', createMarkup(images));
-  const gallery = new SimpleLightbox('.gallery a');
-}
-
-// Галерея и карточка изображения
-
-// {
-// <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>;
-// }
-
-// Библиотека SimpleLightbox
-// // Описан в документации
-// import SimpleLightbox from 'simplelightbox';
-// // Дополнительный импорт стилей
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// Прокрутка страницы
-// const { height: cardHeight } = document
-//   .querySelector('.gallery')
-//   .firstElementChild.getBoundingClientRect();
-
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: 'smooth',
-// });
